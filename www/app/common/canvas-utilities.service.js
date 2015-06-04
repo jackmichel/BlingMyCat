810270c307a234a7028ea787b4a2c49ff2cea673
Jack Michel <jamichel@indiana.edu>
2015-06-04 07:23:42 -0500

    add canvasUtilities service

---
+++ www/app/common/canvas-utilities.service.js
@@ -0,0 +1,46 @@
+angular
+  .module('starter')
+  .factory('canvasUtilities', canvasUtilities);
+
+function canvasUtilities() {
+
+  function createCanvas(element) {
+    var canvasWidth = window.screen.width;
+    var canvasHeight = canvasWidth;
+
+    var canvas = new fabric.Canvas(element, { width: canvasWidth, height: canvasHeight });
+    canvas.setBackgroundColor('rgba(0, 0, 0, 1.0)', canvas.renderAll.bind(canvas));
+
+    return canvas;
+  }
+
+  function addImgSrc(canvas, imgSrc) {
+    var img = document.createElement('img');
+    img.src = imgSrc;
+
+    var imgInstance = new fabric.Image(img, {
+        top: 0,
+        left: 0
+    });
+
+    imgInstance.hasBorders = false;
+    imgInstance.lockRotation = true;
+    imgInstance.lockScalingX = true;
+    imgInstance.lockScalingY = true;
+    // Is the image taller or wider?
+    if (img.width > img.height) {
+      imgInstance.scaleToHeight(canvas.height + 1);
+      imgInstance.lockMovementY = true;
+    } else {
+      imgInstance.scaleToWidth(canvas.width + 1);
+      imgInstance.lockMovementX = true;
+    }
+    canvas.add(imgInstance);
+  } 
+
+  return {
+    createCanvas: createCanvas,
+    addImgSrc: addImgSrc
+  }
+
+}
--- www/app/image-edit/image-editor.directive.js
+++ www/app/image-edit/image-editor.directive.js
@@ -2,39 +2,14 @@ angular
   .module('starter')
   .directive('imageEditor', imageEditor);
 
-function imageEditor($cordovaFile, imageStore, blob, utilities) {
+function imageEditor($cordovaFile, imageStore, blob, utilities, canvasUtilities) {
   return {
     restrict: 'E',
     scope: { currentImage: '=' },
     template: '<canvas></canvas><button>Save</button>',
     link: function(scope, element, attr) {
-      var canvasWidth = window.screen.width;
-      var canvasHeight = canvasWidth;
-
-      var canvas = new fabric.Canvas(element.find('canvas')[0], { width: canvasWidth, height: canvasHeight });
-      canvas.setBackgroundColor('rgba(0, 0, 0, 1.0)', canvas.renderAll.bind(canvas));
-
-      var img = document.createElement('img');
-      img.src = cordova.file.dataDirectory + scope.currentImage;
-
-      var imgInstance = new fabric.Image(img, {
-          top: 0,
-          left: 0
-      });
-
-      imgInstance.hasBorders = false;
-      imgInstance.lockRotation = true;
-      imgInstance.lockScalingX = true;
-      imgInstance.lockScalingY = true;
-      // Is the image taller or wider?
-      if (img.width > img.height) {
-        imgInstance.scaleToHeight(canvasHeight + 1);
-        imgInstance.lockMovementY = true;
-      } else {
-        imgInstance.scaleToWidth(canvasWidth + 1);
-        imgInstance.lockMovementX = true;
-      }
-      canvas.add(imgInstance);
+      var canvas = canvasUtilities.createCanvas(element.find('canvas')[0]);
+      canvasUtilities.addImgSrc(canvas, cordova.file.dataDirectory + scope.currentImage);
 
       element.find('button').on('click', function() {
         var imageBlob = blob.canvasToBlob(canvas);
