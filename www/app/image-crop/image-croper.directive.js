102adf6f8839d75d01799e4c61bab35120d65fcc
Jack Michel <jamichel@indiana.edu>
2015-06-07 13:23:23 -0500

    refactor directives

--- www/app/image-crop/image-croper.directive.js
+++ www/app/image-crop/image-croper.directive.js
@@ -6,24 +6,30 @@
     .directive('imageCroper', imageCroper);
 
   function imageCroper($cordovaFile, imageStore, blob, utilities, canvasUtilities) {
+
     return {
       restrict: 'E',
       scope: { currentImage: '=' },
       template: '<canvas></canvas><button class="button button-block button-energized">Save</button>',
-      link: function(scope, element, attr) {
-        var canvas = canvasUtilities.createCanvas(element.find('canvas')[0]);
-        canvasUtilities.addImgDataUrl(canvas, scope.currentImage);
+      link: link
+    };
 
-        element.find('button').on('click', function() {
-          var imageBlob = blob.canvasToBlob(canvas);
-          var newName = utilities.makeId();
+    /////////////
 
-          $cordovaFile.writeFile(cordova.file.dataDirectory, newName, imageBlob, true).then(function(e) {
-            imageStore.storeImage(newName);
-          });
+    function link(scope, element, attr) {
+      var canvas = canvasUtilities.createCanvas(element.find('canvas')[0]);
+      canvasUtilities.addImgDataUrl(canvas, scope.currentImage);
+
+      element.find('button').on('click', function() {
+        var imageBlob = blob.canvasToBlob(canvas);
+        var newName = utilities.makeId();
+
+        $cordovaFile.writeFile(cordova.file.dataDirectory, newName, imageBlob, true).then(function(e) {
+          imageStore.storeImage(newName);
         });
-      }
+      });
     }
+
   }
 
 })();
--- www/app/image-edit/image-editor.directive.js
+++ www/app/image-edit/image-editor.directive.js
@@ -6,24 +6,30 @@
     .directive('imageEditor', imageEditor);
 
   function imageEditor($cordovaFile, imageStore, blob, utilities, canvasUtilities) {
+
     return {
       restrict: 'E',
       scope: { currentImage: '=' },
       template: '<canvas></canvas><button>Save</button>',
-      link: function(scope, element, attr) {
-        var canvas = canvasUtilities.createCanvas(element.find('canvas')[0]);
-        canvasUtilities.addImgSrc(canvas, scope.currentImage);
+      link: link
+    };
 
-        element.find('button').on('click', function() {
-          var imageBlob = blob.canvasToBlob(canvas);
-          var newName = utilities.makeId();
+    ////////////
 
-          $cordovaFile.writeFile(cordova.file.dataDirectory, newName, imageBlob, true).then(function(e) {
-            imageStore.storeImage(newName);
-          });
+    function link(scope, element, attr) {
+      var canvas = canvasUtilities.createCanvas(element.find('canvas')[0]);
+      canvasUtilities.addImgSrc(canvas, scope.currentImage);
+
+      element.find('button').on('click', function() {
+        var imageBlob = blob.canvasToBlob(canvas);
+        var newName = utilities.makeId();
+
+        $cordovaFile.writeFile(cordova.file.dataDirectory, newName, imageBlob, true).then(function(e) {
+          imageStore.storeImage(newName);
         });
-      }
+      });
     }
+
   }
 
 })();
