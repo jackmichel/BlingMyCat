192ce970dcbce14c69bb1b35d313a37a7ae4fb2c
Jack Michel <jamichel@indiana.edu>
2015-08-17 06:26:09 -0500

    Add confirm to go home

--- www/app/image-edit/image-edit.controller.js
+++ www/app/image-edit/image-edit.controller.js
@@ -5,10 +5,27 @@
     .module('BlingMyCat')
     .controller('ImageEditCtrl', ImageEditCtrl);
 
-  function ImageEditCtrl($stateParams, camera) {
+  function ImageEditCtrl($stateParams, camera, $state, $ionicViewSwitcher, $ionicPopup) {
     var vm = this;
 
     vm.currentImage = $stateParams.image;
+    vm.goHome = goHome;
+
+    function goHome() {
+      var confirm = $ionicPopup.confirm({
+        title: 'Go home without saving?',
+        subTitle: 'Are you sure you want to go home? Your image will not be saved.',
+        okText: 'Yes',
+        okType: 'button-energized',
+        cancelText: 'Cancel'
+      });
+      confirm.then(function (answer) {
+        if (answer) {
+          $ionicViewSwitcher.nextDirection('back');
+          $state.go('tab.image-view');
+        }
+      });
+    }
   }
 
 })();
